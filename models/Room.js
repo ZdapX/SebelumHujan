4746885e2f6b26be333f733fd7ec9b99941c5906
ZdapX <stokmlmurmer150@gmail.com>
2026-02-02 07:35:25 +0700

    Create Room.js

---
+++ models/Room.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const roomSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  description: {
+    type: String,
+    default: ''
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  users: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export default mongoose.models.Room || mongoose.model('Room', roomSchema);
